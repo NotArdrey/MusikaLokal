20c9a75544aea191bdb94746f8f005ec82839868
Alen <araragisakamoto14@gmail.com>
2025-11-07 08:54:32 +0800

    fix(Header): properly align header icons and add spacing
    - Removed fixed-width wrappers around icons
    - Replaced ineffective Tailwind `space-x-4` with `mr-4` for spacing
    - Used `flex-row` and `justify-between` to properly align back button, title, profile, and notification icons
    - Ensures consistent spacing and layout on all screen sizes

---
+++ app/explore.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Header from '../components/header';
+
+
+
+export default function ExploreScreen() {
+  return (
+    <View className="flex-1 bg-white px-6">
+        <Header title ="Explore"/>
+
+    </View>
+  );
+}
--- components/header.jsx
+++ components/header.jsx
@@ -1,20 +1,65 @@
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import { usePathname } from "expo-router";
+import React, { useEffect, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
+
 
 export default function Header({ title}) {
 
+    const pathname = usePathname();
+    const [backVisible, setBackVisible] = useState(false);
+    const [profileVisible, setProfileVisible] = useState(false);
+    const [notifVisible, setnotifVisible] = useState(false);
     const navigation = useNavigation();
 
+    useEffect(() => {
+        if (pathname === "/explore" || pathname === "/home" || pathname === "/manage" ){
+            setProfileVisible(true)
+            setnotifVisible(true)
+            setBackVisible(false)           
+        }else{
+            setBackVisible(true)
+            setProfileVisible(false)
+            setnotifVisible(false)           
+        }
+    }, [pathname]);
+        
+
     return (
-    <View className="flex-row bg-white items-center pt-8">
-        <TouchableOpacity onPress={() => navigation.goBack()} className="z-10">
-            <Ionicons name ="arrow-back" size={24} color="black"/>
-        </TouchableOpacity>
-        <View className="absolute left-0 right-0 items-center">
+    <View className="flex-row bg-white justify-between items-center pt-8 px-4 gap-2">
+        {/*back button. Conditional of visibility*/}
+        <View className="w-1/4">
+        {backVisible&&(
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Ionicons name ="arrow-back" size={24} color="black"/>
+            </TouchableOpacity>
+        )}
+        </View>
+
+        <View className="w-1/4">
+            {/*profile icon. Conditional. navigation still not yet set*/}
+            {profileVisible&&(
+                <TouchableOpacity onPress={() =>navigation.goBack()} >
+                    <FontAwesome name="user-circle" size={28} color="black" />
+                </TouchableOpacity>
+            )}
+        </View>
+
+        {/*Title of the Page in the center*/}
+        <View className="flex-1 items-center">
             <Text className="text-black text-xl" style={{ fontFamily: 'Poppins_600SemiBold' }}>{title}</Text>
         </View>
+
+        <View className="w-1/4 flex-row justify-end">
+            {/*notification icon. Conditional. navigation not yet set*/}
+            {notifVisible&&(
+                <TouchableOpacity onPress = {() => navigation.goBack()}>
+                    <MaterialIcons name="notifications-none" size={33} color="black" />
+                </TouchableOpacity>
+            )}
+        </View>
+
     </View>
     );
 }
